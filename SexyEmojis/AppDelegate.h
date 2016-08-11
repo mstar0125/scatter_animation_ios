1faab2a78084a60b8c253948c6faa289eb447a05
MSTAR <mstar0125@gmail.com>
2016-08-11 13:21:48 -0500

    initial commit

---
+++ SexyEmojis/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SexyEmojis
+//
+//  Created by Hexi Zhang on 4/30/14.
+//  Copyright (c) 2014 XZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
